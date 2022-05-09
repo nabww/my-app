5f8ebfd83dd7db02cd49337e5e58624610c99bc2
nabww <nwambulwa@yahoo.com>
2022-05-09 10:29:13 +0300

    5th

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from "react"
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Heading Up</h1>
+        <p>This is A react Component in paragraph</p>
+      </React.Fragment>
+    )
+  }
+}
+export default TodoContainer
