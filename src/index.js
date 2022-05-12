ea66d49d816488d8c4d36498bacdd1ffe2d2ac72
nabww <nwambulwa@yahoo.com>
2022-05-12 17:42:02 +0300

    12th May

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import "./app.css";
-//import * as ReactDOM from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 
 //component file
 import TodoContainer from "./components/TodoContainer";
